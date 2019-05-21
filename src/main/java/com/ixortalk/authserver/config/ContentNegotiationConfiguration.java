85451b553bdf7e9c2a8d4ff1e84e82914f68af34
Ward Jans <ward.jans@wja-consulting.be>
2019-05-21 15:43:09 +0200

    Closes #45: Setting favor path extension to false

---
+++ src/main/java/com/ixortalk/authserver/config/ContentNegotiationConfiguration.java
@@ -0,0 +1,14 @@
+package com.ixortalk.authserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class ContentNegotiationConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false);
+    }
+}
